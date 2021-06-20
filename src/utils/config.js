b3c51f68894c1ca728a8c324c65fdf94d9bb0ac2
obrin <jordanyong23@gmail.com>
2021-06-20 09:48:44 +0200

    debug

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 import { UnleashClient } from 'unleash-proxy-client'
 
-console.log('process', process.env.REACT_APP_UNLEASH_KEY)
+// console.log('process', process.env.REACT_APP_UNLEASH_KEY)
 const config = new UnleashClient({
-  // url: 'http://config-proxy.home.local/proxy',
-  // clientKey: process.env.REACT_APP_UNLEASH_KEY,
-  url: 'http://unleash.herokuapp.com/api/',
+  url: 'http://config-proxy.home.local/proxy',
+  clientKey: process.env.REACT_APP_UNLEASH_KEY || 'secret',
+  // url: 'http://unleash.herokuapp.com/api/',
   clientKey: '3bd74da5b341d868443134377ba5d802ea1e6fa2d2a948276ade1f092bec8d92',
   appName: 'sandbox-frontend-01',
   refreshInterval: 2,
